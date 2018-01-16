b7daeed640b9dceaa62bf6bfeadd2bae9a756b7b
Unknown <briggscalum@gmail.com>
2018-01-16 16:15:54 -0500

    Merge of Animation

--- planpathsmall.py
+++ planpathsmall.py
@@ -16,6 +16,9 @@ R = np.matrix('{} {}; {} {}'.format(c, -s, s, c))
 
 distance = [0] * 100
 frames = [[]]
+frames2 = [[]]
+frames3 = [[]]
+frames4 = [[]]
 
 initialPos1 = [2, 0];
 initialPos2 = [4, 0];
@@ -85,14 +88,26 @@ for index in range (0,99):
 for index in range (0,99):
 	path1x[index] = distance1[index]*math.cos(angle1[index]);
 	path1y[index] = distance1[index]*math.sin(angle1[index]);
+	frames2.append(plt.Line2D(path1x[0:index],path1y[0:index]));
 
 # Create Path 2
 for index in range (0,99):
 	path2x[index] = distance2[index]*math.cos(angle2[index]);
 	path2y[index] = distance2[index]*math.sin(angle2[index]);
+	frames3.append(plt.Line2D(path2x[0:index],path2y[0:index]));
+
+
+# Create Drawn Path attempt 2
+
+realpathx = [0] * 100
+realpathy = [0] * 100
+
+for index in range (0,99):
+	realpathx[index] = path2x[index] - (path1x[index] - path2x[index])
+	realpathy[index] = path2y[index] - (path1y[index] - path2y[index])
+	frames4.append(plt.Line2D(realpathx[0:index],realpathy[0:index]));
 
 # Create Drawn Path
-
 sewpathx  = [0] * 100
 sewpathy  = [0] * 100
 ratio = 0;
@@ -115,28 +130,35 @@ for index in range (0,100):
 line = plt.Line2D([0,0],[0,0], color = 'm')
 line.set_data(frames[35].get_data())
 
-ax.set_xlim([-5,5])
-ax.set_ylim([-5,5])
+ax.set_xlim([-10,10])
+ax.set_ylim([-10,10])
 
 x = np.arange(0, 2*np.pi, 0.01)
 line, = ax.plot(x, np.sin(x))
-
+line2,  = ax.plot(x, np.sin(x))
+line3,   = ax.plot(x, np.sin(x))
+line4,   = ax.plot(x, np.sin(x))
 
 def animate(i):
     line.set_ydata(frames[i].get_ydata())  # update the data
     line.set_xdata(frames[i].get_xdata())  # update the data
-    return line,
+    line2.set_ydata(frames2[i].get_ydata())  # update the data
+    line2.set_xdata(frames2[i].get_xdata())  # update the data
+    line3.set_ydata(frames3[i].get_ydata())  # update the data
+    line3.set_xdata(frames3[i].get_xdata())  # update the data
+    line4.set_ydata(frames4[i].get_ydata())  # update the data
+    line4.set_xdata(frames4[i].get_xdata())  # update the data
+    return line,line2,line3,line4
 
 
 # Init only required for blitting to give a clean slate.
 def init():
     line.set_ydata(np.ma.array(x, mask=True))
-    return line,
+    return line,line2,line3,line4
 
-ani = animation.FuncAnimation(fig, animate, np.arange(4, 100), init_func=init,
-                              interval=100, blit=True)
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 100), init_func=init,interval=100, blit=True)
 
-ani = animation.FuncAnimation(fig, animate, np.arange(4, 100), init_func=init, interval=50, blit=True)
+#ani = animation.FuncAnimation(fig, animate, np.arange(4, 100), init_func=init, interval=50, blit=True)
 
 #ani.save('SewPath.mp4', writer=writer)
 plt.show()
