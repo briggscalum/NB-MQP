ef204641b6b0db489144107f12e4ffc50dfcf6a2
briggscalum <briggscalum@gmail.com>
2018-03-12 14:42:12 -0400

    cleaned up code

--- PathPlanner.py
+++ PathPlanner.py
@@ -26,22 +26,6 @@ canvassize = [0.2,0.3]
 hand2pos = [0.1,0.0]
 hand1pos = [0.2, 0.0]
 
-
-#calculate intital distance and angle
-intialdist1 =  math.hypot(hand1pos[1], canvassize[0]/2 -hand1pos[0])
-intialdist2 =  math.hypot(hand2pos[1], canvassize[0]/2 -hand2pos[0])
-
-intialangle1 =	np.arctan2(( canvassize[0]/2 -hand1pos[0]) , (hand1pos[1]));
-intialangle2 =  np.arctan2(( canvassize[0]/2 -hand2pos[0]) , (hand2pos[1]));
-
-
-# Generate line
-# curve_name = 'sinecurve3.png'
-# wavex,wavey = vision.load_and_show(curve_name)
-# wavex = np.asfarray(wavex)
-# wavey = np.asfarray(wavey)
-
-
 wavex = [0] * 500;
 wavey = [0] * 500;
 
@@ -51,7 +35,6 @@ for index in range (0,500):
 
 wavex, wavey = scaleplot(wavex,wavey,canvassize)
 
-
 plt.plot(wavex,wavey);
 ax.plot(hand1pos[0],hand1pos[1], marker='o', markersize=3, color="red")
 ax.plot(hand2pos[0],hand2pos[1], marker='o', markersize=3, color="green")
@@ -59,17 +42,7 @@ ax.plot(hand2pos[0],hand2pos[1], marker='o', markersize=3, color="green")
 
 length=(len(wavex))
 
-# wavex=np.flip(wavex,0)
-# wavey=np.flip(wavey,0)
-# wavex=wavex[0:length-1:1]
-# wavey=wavey[0:length-1:1]
-
-length=len(wavey)
 waveangle = [0] * length
-
-# Angle between path point and grip point
-pureangle1 = [0] * length
-pureangle2 = [0] * length
 
 # Adjusted Angle between path point and grip point to be written
 angle1 = [0] * length
@@ -91,17 +64,9 @@ hand1pathx = [0] * length
 hand1pathy = [0] * length
 hand2pathx = [0] * length
 hand2pathy = [0] * length
-hand1dist = math.sqrt(hand1pos[0]**2 + hand1pos[1]**2)
-hand2dist = math.sqrt(hand2pos[0]**2 + hand2pos[1]**2)
 
-#hand1pos[0] = hand1dist * cos(waveangle[0] - np.arctan2((wavex[0] - hand1pos[0]) , (wavey[0] - hand1pos[1])));
-#hand1pos[1] = hand1dist * sin(waveangle[0] - np.arctan2((wavex[0] - hand1pos[0]) , (wavey[0] - hand1pos[1])));
- 
 hand1pos[0], hand1pos[1] = rotatepoint(hand1pos[0], hand1pos[1], 2*math.pi/3 - waveangle[0])
 hand2pos[0], hand2pos[1] = rotatepoint(hand2pos[0], hand2pos[1], 2*math.pi/3 - waveangle[0])
-
-# hand2pos[0] = hand2dist * cos(waveangle[0] - np.arctan2((wavex[0] - hand2pos[0]) , (wavey[0] - hand2pos[1])));
-# hand2pos[1] = hand2dist * sin(waveangle[0] - np.arctan2((wavex[0] - hand2pos[0]) , (wavey[0] - hand2pos[1])));
 
 newhand1path = {0.0,0.0}
 newhand2path = {0.0,0.0}
@@ -136,15 +101,12 @@ for index in range (0,length):
 	frames3.append(plt.Line2D(hand2pathx[1:index],hand2pathy[1:index]))
 	#frames4.append(ax.plot(hand1pathx[0:index], hand1pathy[0:index], 'o'))
 
-	#frames2.append(plt.Line2D([sewpathx[0]+intialdist1*math.cos(waveangle[index] + intialangle1 - math.pi/4) , 0.7], [sewpathy[0]+intialdist1*math.sin(waveangle[index] + intialangle1 - math.pi/4) , 0]));
-	#frames3.append(plt.Line2D([sewpathx[0]+intialdist2*math.cos(waveangle[index] + intialangle2 - math.pi/4) , -0.7],[sewpathy[0]+intialdist2*math.sin(waveangle[index] + intialangle2 - math.pi/4) , 0]));
-
 # line = plt.Line2D([0,0],[0,0], color = 'm')
 # line.set_data(frames[35].get_data())
 
 ax.axis('equal')
-ax.set_xlim([-0.5,0.5])
-ax.set_ylim([-0.5,0.5])
+#ax.set_xlim([-0.5,0.5])
+#ax.set_ylim([-0.5,0.5])
 
 
 x = np.arange(0, 2*np.pi, 2*np.pi/length)
@@ -164,7 +126,7 @@ def animate(i):
 # Init only required for blitting to give a clean slate.
 def init():
 	line.set_ydata(np.ma.array(x, mask=True))
-	return line,line2,line3,
+	return line,line2,line3,point,
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, length), init_func=init,interval=50, blit=True)
 
